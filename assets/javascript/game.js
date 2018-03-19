726d282450802871f85fdeb8b263b7f19921a090
Edward Nguyen <ednguyen92@gmail.com>
2018-03-18 18:31:36 -0700

    finished most of code need to work out some kinks with reseting the game and random numbers for gems

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,50 @@
 // Global variables
+var gemsArray = ["assets/images/Gem_One.jpg", "assets/images/Gem_Two.jpg", "assets/images/Gem_Three.jpg" ,"assets/images/Gem_Four.jpg"]
 var wins = 0;
 var losses = 0;
 var targetNumber = 0;
-var totalScore = 0;
 var counterNumber = 0;
 var gemValue = [10, 5, 3, 7];
 var randGemValue = [];
-var increment = 0;
-var gemsArray = ["assets/images/Gem_One.jpg", "assets/images/Gem_Two.jpg", "assets/images/Gem_Three.jpg" ,"assets/images/Gem_Four.jpg"]
+var crystals = $("#crystals");
+
 
 // start game function
 function startGame() {
-    targetNumber = "";
-    totalScore = 0;
-    increment = "";
+    targetNumber = 0;
+    counterNumber = 0;
+    randGemValue = [];
     
-    // increment = gemValue[Math.floor(Math.random() * gemValue.length)];
     targetNumber = Math.floor(Math.random() * 61) + 40;
-    
-
-    $("#guess_number").text(targetNumber);
+    $("#guess-number").text(targetNumber);
+    $("#currentScore").text(counterNumber);
+    getGemNumber();
     console.log("Start game working");
 }
 
+// reset game function
+// relist variable to start on clean slate
+function resetGame() {
+    targetNumber = 0;
+    counterNumber = 0;
+    randGemValue = [];
+    imageCrystal.attr();
+  
+
+    startGame();
+    console.log("reset game working");
+}
+
 function getGemNumber() {
-    for (var i = 0; i < gemValue.length; i++) {
-        randGemValue.push(gemValue[Math.round(Math.random())]);
+    for (var i = 0; i < 4; i++) {
+        randGemValue.push(gemValue[Math.round(Math.random() * gemValue.length)]);
     }
     console.log(randGemValue);
     for (var i = 0; i < gemValue.length; i++) {
         var imageCrystal = $("<img>");
         imageCrystal.addClass("crystal-image");
         imageCrystal.attr("src", gemsArray[i]);
-        imageCrystal.attr("data-crystalvalue", gemValue[i]);
+        imageCrystal.attr("data-crystalvalue", randGemValue[i]);
     
         // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
         crystals.append(imageCrystal);
@@ -40,29 +52,30 @@ function getGemNumber() {
 
 }
 
-
-
-// reset game function
-
-// relist variable to start on clean slate
-// random number generator to generate random number to achieve
 startGame();
-getGemNumber();
-
-
-
 
 // on click for each crystal
- /*$("#gem_container").on("click", ".gemOne", function() {
-    totalScore += increment;
-    $("#currentScore").text(totalScore);
-    console.log("Gem One Working"); 
-}).on */
+crystals.on("click", ".crystal-image", function() {
 
-/* $("#crystals").on("click", ".crystal-image", function() {
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
-    totalScore += crystalValue;
-}); */
+    counterNumber += crystalValue;
+    $("#currentScore").text(counterNumber);
+
+    if (counterNumber === targetNumber) {
+      wins++;
+      $("#wins").text(wins);
+      resetGame();
+    }
+
+    else if (counterNumber >= targetNumber) {
+      losses++;
+      $("#losses").text(losses);
+      resetGame();
+    }
+
+  });
+
+
 
 
