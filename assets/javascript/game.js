5a73702c5448a7d474552826f2e23d6141008c26
Edward Nguyen <ednguyen92@gmail.com>
2018-03-20 14:39:25 -0700

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 // Global variables
-var gemsArray = ["assets/images/Gem_One.jpg", "assets/images/Gem_Two.jpg", "assets/images/Gem_Three.jpg" ,"assets/images/Gem_Four.jpg"]
 var wins = 0;
 var losses = 0;
 var targetNumber = 0;
 var counterNumber = 0;
 var gemValue = [10, 5, 3, 7];
 var randGemValue = [];
+var gemsArray = ["assets/images/Gem_One.jpg", "assets/images/Gem_Two.jpg", "assets/images/Gem_Three.jpg" ,"assets/images/Gem_Four.jpg"]
 var crystals = $("#crystals");
 
 
@@ -14,6 +14,7 @@ function startGame() {
     targetNumber = 0;
     counterNumber = 0;
     randGemValue = [];
+    
     
     targetNumber = Math.floor(Math.random() * 61) + 40;
     $("#guess-number").text(targetNumber);
@@ -28,7 +29,6 @@ function resetGame() {
     targetNumber = 0;
     counterNumber = 0;
     randGemValue = [];
-    imageCrystal.attr();
   
 
     startGame();
@@ -37,7 +37,7 @@ function resetGame() {
 
 function getGemNumber() {
     for (var i = 0; i < 4; i++) {
-        randGemValue.push(gemValue[Math.round(Math.random() * gemValue.length)]);
+        randGemValue.push(gemValue[Math.floor(Math.random() * gemValue.length)]);
     }
     console.log(randGemValue);
     for (var i = 0; i < gemValue.length; i++) {
